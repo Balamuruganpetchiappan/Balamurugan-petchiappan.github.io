dc7a5f9aa91e1cd460d3acd06213b40dc59be6a0
Balamurugan Petchiappan <98416961+Balamuruganpetchiappan@users.noreply.github.com>
2025-06-06 01:17:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/demo_hackathon/',
+  plugins: [vue()],
 })
+
